8b532e1390196a25a3c0a1d64dc7bd77aa501896
Roman Pavlenko <rpavlenko.mail@gmail.com>
2025-08-18 17:30:19 +0200

    feat: modify text color, add class name

--- 3337875578486/scripts/script.js
+++ 3337875578486/scripts/script.js
@@ -1,21 +1,21 @@
 (function () {
-  const root = document.querySelector("#es-rc-content");
+  const root = document.querySelector('#es-rc-content');
   if (!root) return;
 
-  const accTab = root.querySelectorAll(".es-acc-tab");
+  const accTab = root.querySelectorAll('.es-acc-tab');
   for (let i = 0; i < accTab.length; i++) {
-    accTab[i].addEventListener("click", function () {
-      this.classList.add("active");
+    accTab[i].addEventListener('click', function () {
+      this.classList.add('es-active');
       let accTabContent = this.nextElementSibling;
 
       const hideContent = () => {
         accTabContent.style.maxHeight = null;
-        this.classList.remove("active");
+        this.classList.remove('es-active');
       };
 
       const showContent = () => {
-        accTabContent.style.maxHeight = accTabContent.scrollHeight + "px";
-        this.classList.add("active");
+        accTabContent.style.maxHeight = accTabContent.scrollHeight + 'px';
+        this.classList.add('es-active');
       };
 
       if (accTabContent.style.maxHeight) {
@@ -26,33 +26,33 @@
     });
   }
 
-  const swiper = new Swiper(root.querySelector(".es-swiper1"), {
-    direction: "horizontal",
+  const swiper = new Swiper(root.querySelector('.es-swiper1'), {
+    direction: 'horizontal',
     loop: true,
     pagination: {
-      el: root.querySelector(".es-swiper1-pagination"),
+      el: root.querySelector('.es-swiper1-pagination'),
     },
     navigation: {
-      nextEl: root.querySelector(".es-swiper1-button-next"),
-      prevEl: root.querySelector(".es-swiper1-button-prev"),
+      nextEl: root.querySelector('.es-swiper1-button-next'),
+      prevEl: root.querySelector('.es-swiper1-button-prev'),
     },
   });
 
-  const swiper2 = new Swiper(root.querySelector(".es-swiper2"), {
-    direction: "horizontal",
+  const swiper2 = new Swiper(root.querySelector('.es-swiper2'), {
+    direction: 'horizontal',
     loop: true,
     pagination: {
-      el: root.querySelector(".es-swiper2-pagination"),
+      el: root.querySelector('.es-swiper2-pagination'),
     },
     navigation: {
-      nextEl: ".es-swiper2-button-next",
-      prevEl: ".es-swiper2-button-prev",
+      nextEl: '.es-swiper2-button-next',
+      prevEl: '.es-swiper2-button-prev',
     },
   });
 
-  const allNextBtns = root.querySelectorAll(".es-swiper2-button-next");
+  const allNextBtns = root.querySelectorAll('.es-swiper2-button-next');
   allNextBtns.forEach((btn) => {
-    btn.addEventListener("click", () => {
+    btn.addEventListener('click', () => {
       swiper2.slideNext();
     });
   });
